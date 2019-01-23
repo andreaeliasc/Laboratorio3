9716174966a13fdbab2e7799732d049411cca305
DESKTOP-DMEFEJ6\Andrea <andrea.elias16@gmail.com>
2019-01-23 08:22:01 -0600

    Layouts completos

--- app/src/main/java/com/example/andrea/laboratorio/Contactos.kt
+++ app/src/main/java/com/example/andrea/laboratorio/Contactos.kt
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class Contactos : AppCompatActivity() {
-
+    val contactos: ArrayList<String> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contactos)
+
+
+
+
     }
 }
--- app/src/main/java/com/example/andrea/laboratorio/Enviar.kt
+++ app/src/main/java/com/example/andrea/laboratorio/Enviar.kt
@@ -3,10 +3,10 @@ package com.example.andrea.laboratorio
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class Email : AppCompatActivity() {
+class Enviar : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_email)
+        setContentView(R.layout.activity_enviar)
     }
 }
