29500a8065689e65a91ab8a11baf2dee4283eb13
DESKTOP-DMEFEJ6\Andrea <andrea.elias16@gmail.com>
2019-01-23 07:19:45 -0600

    Creacion del proyecto

---
+++ Lab3/app/src/main/java/com/example/andrea/lab3/CrearContacto.kt
@@ -0,0 +1,12 @@
+package com.example.andrea.lab3
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearContacto : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_contacto)
+    }
+}
